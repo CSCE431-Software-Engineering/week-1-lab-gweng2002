3f94456402ef99b8a2c4704be9f5b04ff97a66a7
root <root@localhost>
2024-01-31 07:01:11 +0000

    pre browser quality testing

---
+++ build_blog/app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    flash[:notice] = "Book was successfully deleted."
+    redirect_to books_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:title)
+    end
+  
+end
--- build_blog/config/routes.rb
+++ build_blog/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'books#index'
+  resources :books
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/create'
+  get 'books/update'
+  get 'books/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
